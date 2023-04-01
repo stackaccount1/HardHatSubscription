7aa1e24b4d6f075264cfb30e14d59abd3cbc5d38
Ira <iraf333@gmail.com>
2023-04-01 01:00:01 -0400

    add chainlink price feeds in

--- hardhat-starter-kit/contracts/Subscription.sol
+++ hardhat-starter-kit/contracts/Subscription.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (token/ERC20/ERC20.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 // Subscription Monthly Payment Contract
-// 1. Contract is controlled by chainlink keepers
-// 2. Monthly Epoch is incremented for eternity by gelato service or manually -> if manually, a delay in updating updateEpoch only allows customers to get more time for what they paid
-// 3. A check can be done on the delta of monthly epoch from month of origination
-// 4.
+// -> Contract is controlled by chainlink keepers
+// -> Monthly Epoch is incremented for eternity by gelato service or manually -> if manually, a delay in updating updateEpoch only allows customers to get more time for what they paid
+// -> A check can be done on the delta of monthly epoch from month of origination
+// ->
+
+// 2. Imports
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract Subscription {
+    using PriceConverter for uint256;
     //State Variables
     address private immutable i_owner;
     //For Reference
@@ -24,6 +29,8 @@ contract Subscription {
     mapping(address => uint256) epochPaid;
     // usd pricing
     uint usdPrice;
+    //comment
+    AggregatorV3Interface private s_priceFeed;
 
     modifier onlyOwner() {
         require(msg.sender == i_owner, "Only owner can call this function.");
@@ -31,11 +38,12 @@ contract Subscription {
     }
 
     //epoch starts at 0
-    constructor(uint256 _initialSubPrice) {
+    constructor(uint256 _initialSubPrice, address priceFeed) {
         i_owner = msg.sender;
         epoch = 0;
         i_start = block.timestamp;
         usdPrice = _initialSubPrice;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     function updateEpoch() public onlyOwner {
@@ -51,8 +59,11 @@ contract Subscription {
     }
 
     function _makePayment(address _recievooor, uint256 _periods) internal {
-        uint value = 1 ether * _periods;
-        require(msg.value >= value, "not enough ether submitted");
+        uint value = usdPrice * _periods;
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= value,
+            "not enough ether submitted"
+        );
         // EpochPaid updated to epoch + 1 on minimum paid; user can game system letting the second epoch month expire then paying at the beginning the 3rd epoch, extending through to 4 epochs for the price of two
         // A user paying inside of the middle or end of the month benefits positively from this lets say gets a month and a half for the price of one if paid on the 15th
         // Ideally a user will pay several months ahead of time or years for convienence
@@ -67,15 +78,21 @@ contract Subscription {
         uint256 _periods
     ) internal {
         if (_periods > 3) {
-            uint value = 1 ether * _periods;
-            require(msg.value >= value, "not enough ether submitted");
+            uint value = usdPrice * _periods;
+            require(
+                msg.value.getConversionRate(s_priceFeed) >= value,
+                "not enough ether submitted"
+            );
             epochPaid[_recievooor] = epoch + _periods;
         } else {
             // Making a payment <3 will result in double the cost
             // For simplicity this seems as the best idea than trying to figure timing inside the contract
             // pricing should be adjusted accordingly for the loss of 1 month every 4 if the customer is paying every 4 months on the end of the fifth month after expiry of epoch..........
-            uint valueDoubler = 2 ether * _periods;
-            require(msg.value >= valueDoubler, "not enough ether submitted");
+            uint valueDoubler = 2 * usdPrice * _periods;
+            require(
+                msg.value.getConversionRate(s_priceFeed) >= valueDoubler,
+                "not enough ether submitted"
+            );
             epochPaid[_recievooor] = epoch + _periods;
         }
     }
